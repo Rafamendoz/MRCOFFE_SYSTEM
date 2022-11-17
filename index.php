573e4f73eb11e1e235d4db6c9564e9fdbd27ba4e
Helen Madeleine Orellana Galo <you@example.com>
2022-11-16 19:18:52 -0600

    Diseño de Login

--- index.php
+++ index.php
@@ -4,9 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
 </head>
 <body>
-    
+    <section class="inicio">
+        <div class="imagen">
+
+        </div>
+        <div class="forma">
+            <h1 id="title-2">LOGIN</h1>
+                <form action="">
+                    <div class="input">
+                        <input type="text" name="usuario"> 
+                    </div>
+                    <div class="label">
+                        <label for="nombre">USER</label>
+                    </div>
+                    <div class="input">
+                        <input type="password" name="contra" > 
+                    </div>
+                    <div class="label">
+                        <label for="nombre">PASSWORD</label>
+                    </div>
+
+                    <div class="btn">
+                            <input type="submit" value="LOGIN">
+                    </div>
+                    
+                </form>
+
+        </div>
+
+    </section>
 </body>
 </html>
