1f07e194debc10ad8afd29a7f83fa5058fc473c8
Helen Madeleine Orellana Galo <you@example.com>
2022-11-26 02:41:04 -0600

    avance de proyecto

--- restablecer.php
+++ restablecer.php
@@ -4,12 +4,28 @@ require 'funciones.php';
 
 $errors = array();
 if(!empty($_POST)){
+    $email=$mysqli -> real_escape_string($_POST['email']);
 
     if(!isEmail($email)){
 
         $errors[]="Debe ingresar un correo electronico valido";
         if(emailExiste($email)){
-        
+            user_id = getValor('id','correo',$email);
+           nombre = getValor('nombre','correo',$email);
+
+           $token - generaTokenPass($email);
+           $url='http://'.$_SERVER["SERVER_NAME"].'/login/cambia_pass.php?user_id='.$user_id.'&token='.$token;
+           $asunto='Recuperar Password';
+           $cuerpo="Hola $nombre:<br/><br/> Se ha solicitado un reinicio de  contrase&ntilde;a, visita la siguiente direcci&oacute;n: <a href='$url'>$url</a>";
+
+           if(enviarEmail($email,$nombre, $asunto, $cuerpo))    {
+            echo "Hemos enviado un correo electronico a la direccion $email para restablecer tu password. <br/>";
+            echo "<a href='index.php>Iniciar Sesion</a>";
+            exit;
+
+           }else {
+            $errors[] = "Error al Enviar el  Email";
+           }
         }
     }
 }
@@ -19,6 +35,7 @@ if(!empty($_POST)){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,44 +45,64 @@ if(!empty($_POST)){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
         integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link href="css/styles.css" rel="stylesheet" />
-    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Trirong&effect=neon|outline|emboss|shadow-multiple">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-    <div class="container">
-        <div id="loginbox" style=" margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-            <div class="panel panel-info">
-            <div class="panel-heading">
-            <div class="panel-title">Recuperar Password</div>
-            <div style="float:right; font-size: 80%; position:relative; top:-10px"><a href="index.php">Iniciar Sesi&oacute;n</a></div>
+<div class="container d-flex justify-content-center ">
+        <div id="loginbox bg-warning" style="margin-top:15%; margin-left:10%;" class=" card
+        mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-3">
+            <div class="card-body">
+                <div class="card-header">
+                    <div class="panel-title text-warning font-effect-emboss">Recuperar Password</div>
+                    <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="index.php">Iniciar
+                            Sesión</a></div>
+                </div>
 
-        
-        <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+                <div style="padding-top:30px" class="card-body">
 
-        <form  id="loginform" class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF']?>" method="POST" autocomplete="off">
-            <div style="margin-bottom:25px" class="input-group">
-                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                <input  id="email" type="email" class="form-control" name="email" placeholder="email" required>
+                    <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+
+                    <form id="loginform" class="form-horizontal" role="form" action="" method="POST" autocomplete="off">
+
+                        <div style="margin-bottom: 25px" class="input-group">   
+
+                    <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+
+                    <form id="loginform" class="form-horizontal" role="form" action="" method="POST" autocomplete="off">
+
+                        <div style="margin-bottom: 25px" class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input id="email" type="email" class="form-control" name="email" placeholder="email"
+                                required="">
+                        </div>
+
+                        <div style="margin-top:10px" class="form-group">
+                            <div class="col-sm-12 controls">
+                                <button id="btn-login" type="submit" class="btn btn-warning">Enviar
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 control">
+                                <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%">
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <?php echo resultBlock($errors);?>
+                </div>
             </div>
-            <div style="margin-top:10px" class="form-group">
-                <div class="col-sm-12 controls">
-                <button id="btn-login" type ="submit" class="btn btn-success">Enviar</a>
-           </div>
-    </div>
-    <div class="form-group">
-        <div class="col-md-12 control">
-        <div style="border-top:1px solid#888; padding-top:15px; font-size:85%">
         </div>
-         </div>
     </div>
-    </form>
-
-
-    </div>
-    </div>
-    </div>
-    </div>
-    
 </body>
+
 </html>
