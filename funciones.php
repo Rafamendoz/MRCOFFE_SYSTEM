50723c037ed1196135dfff52f9531fcad9dab45b
Edwin Rafael <edwin.espino@ujcv.edu>
2022-11-26 03:57:45 -0600

    ENVIAR CORREO

--- funciones.php
+++ funciones.php
@@ -1,10 +1,13 @@
 <?php
-
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require("vendor/autoload.php");
 
 function emailExiste($email){
     global $mysqli;
 
-    $stmt = $mysqli->prepare("select id from usuarios Where correo =? LIMIT 1");
+    $stmt = $mysqli->prepare("select correo from usuarios Where correo =? LIMIT 1");
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $stmt->store_result();
@@ -21,6 +24,46 @@ function emailExiste($email){
 
 }
 
+function isEmail($email){
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function enviarEmail($email,$nombre, $asunto, $cuerpo){
+    $mail = new PHPMailer(true);
+
+    try {
+      
+        #$mail->SMTPDebug=SMTP::DEBUG_SERVER;#
+        $mail->isSMTP();
+        $mail->Host = 'smtp.titan.email';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'TI@fhope.net';
+        $mail->Password = 'Tifhope2022$$';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587; 
+
+
+        $mail->setFrom('TI@fhope.net', 'Recuperar');
+        $mail->addAddress($email, 'Usuario');
+        $mail->isHTML(true);
+        $mail->Subject= $asunto;
+        $mail->Body=$cuerpo;
+        $mail->send();
+        
+        return true;
+
+    } catch (Exception $th) {
+        echo 'Mensaje'.$mail->ErrorInfo;
+        return false;
+        
+    }
+
+   
+}
 
 
 function getValor($campo, $campoWhere, $valor){
