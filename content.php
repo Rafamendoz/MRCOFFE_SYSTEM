16af053fdf264fe0386b8f3b0f9fa0e14e0d0f7e
daniela120 <daniela.herrera@ujcv.edu.hn>
2022-11-30 17:18:40 -0600

    Layout dinamico

---
+++ content.php
@@ -0,0 +1,19 @@
+<main>
+                
+                <h2> <?php echo "<h4> Bienvenido - ".$_SESSION['User']."</h4>";?></h2>
+                
+
+
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Administrador</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Administrador</li>
+                        </ol>
+                        <div class="row">
+                            
+                        </div>
+                       
+                        
+                    </div>
+                   
+                </main>
---
+++ sidebar.php
@@ -0,0 +1,39 @@
+<!--sidebar-->
+<div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            
+                            <div class="sb-sidenav-menu-heading">Personas</div>
+                            <a <?php if($_SESSION['Rol'] != 1){ ?>  style="display:none;"  <?php } ?> class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div  class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Usuarios
+                                <div class="sb-sidenav-collapse-arrow"></div>
+                            </a>
+                     
+                            <a <?php if($_SESSION['Rol'] != 1){ ?>  style="display:none;"  <?php } ?> class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                                <div  class="sb-nav-link-icon"><i class="fas fa-book-open" ></i></div>
+                                Clientes
+                                <div class="sb-sidenav-collapse-arrow"></div>
+                            </a>
+                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                                
+                            </div>
+                            <div  class="sb-sidenav-menu-heading">Pedidos</div>
+                            <a class="nav-link" href="charts.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                Pedido
+                            </a>
+                            <a class="nav-link" href="tables.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Factura
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
+                    </div>
+                </nav>
+            </div>
